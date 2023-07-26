78eb675accda5a23cad8e55cc70a583e5be5fc71
Dalton <iitzdaltonxd@gmail.com>
2023-07-26 14:11:56 +0100

    Added additional functionality, time properties not applied.

--- DigitalClock.cpp
+++ DigitalClock.cpp
@@ -1,8 +1,28 @@
 
 #include <iostream>
-
+using namespace std;
 int main()
 {
-    std::cout << "Hello World!\n";
+	int hr = 0, min = 0, sec = 0;
+	for (sec < 60; sec += 1;  cout << hr << " : " << min << " : " << sec << endl){
+		if (sec == 60) {
+			sec = 0;
+			min += 1;
+		}
+		if (min == 60) {
+			min = 0;
+			hr += 1;
+
+		}
+		if (hr > 24) {
+			sec = 0;
+			min = 0;
+			hr = 0;
+		}
+    } 
+
 }
 
+
+
+//min < 60; min++; hr < 60; hr++;//
